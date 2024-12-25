14439c619b841afe3c858aacb44d196ac8075a2b
Adam Crass <aj.crass@gmail.com>
2024-12-25 14:58:38 -0800

    added sources page

---
+++ src/components/Sources.jsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+const StyledSources = styled.nav`
+  background-color: #595959;
+  height: 400px;
+`;
+
+const Sources = () => {
+  <StyledSources>
+    <span>https://www.johnjohnsbbqsauce.com</span>
+    <span>
+      https://www.koin.com/sports/sauce-that-even-the-meat-asks-for-by-name-john-johns-bbq/
+    </span>
+  </StyledSources>;
+};
+
+export default Sources;
