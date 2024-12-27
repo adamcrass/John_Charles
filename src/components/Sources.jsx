07381f7e552aeab3155f1262e9d5c5b4608de9eb
Adam Crass <aj.crass@gmail.com>
2024-12-27 09:51:20 -0800

    testing

--- src/components/Sources.jsx
+++ src/components/Sources.jsx
@@ -1,9 +1,37 @@
 import styled from "@emotion/styled";
 
-const StyledSources = styled.div``;
+const StyledSources = styled.div`
+  background-color: #2b2b2b;
+  display: flex;
+  width: 100%;
+  height: 240px;
+  color: #fff;
+  flex-direction: column;
+  padding: 40px;
+  font-size: 50px;
+`;
+
+const SourcesTitle = styled.div`
+  font-size: 40px;
+`;
+
+const SourcesBody = styled.div`
+  font-size: 20px;
+  padding-top: 10px;
+`;
 
 const Sources = () => {
-  <StyledSources>Sources</StyledSources>;
+  return (
+    <StyledSources>
+      <SourcesTitle>High School</SourcesTitle>
+      <SourcesBody>
+        A graduate of Mountain View High School in Mountain View, California,
+        Charles was a three-year starter in football, and baseball. Charles
+        earned All-America honors in football as well as being selected
+        All-League and All-Peninsula in baseball.
+      </SourcesBody>
+    </StyledSources>
+  );
 };
 
 export default Sources;
