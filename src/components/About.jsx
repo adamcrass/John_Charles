d45e2252a1cc8ab4772bd2cc8fd67bea4c8d2245
Adam Crass <aj.crass@gmail.com>
2025-01-26 12:57:24 -0800

    changed highschool and about sections, added meta tags

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,8 +26,27 @@ const About = () => {
       <AboutTitle>About</AboutTitle>
       <AboutText>
         John Charles, a former All-American quarterback, played at Portland
-        State University (PSU) and reached both the National Football League
-        (NFL) and Arena Football League (AFL).
+        State University (PSU) and later reached both the National Football
+        League (NFL) and Arena Football League (AFL). He is the Head Coach of
+        AirOne Football Academy, a premier institution founded in 2012 to
+        develop young football players in Vancouver, Washington, and Portland,
+        Oregon. The academy, under Charles' leadership, focuses on skill
+        development, tactical understanding, and fostering a passion for the
+        game, providing a supportive environment for athletes to thrive.
+        <br />
+        <br />
+        Charles' legacy at PSU includes leading the team to two Western Football
+        Conference Championships and earning All-American honors, culminating in
+        his induction into the Portland State Hall of Fame.
+        <br />
+        <br />
+        Beyond football, Charles is an entrepreneur and mentor. For over a
+        decade, he has coached high school quarterbacks in Oregon and Southwest
+        Washington. During the COVID-19 pandemic, he launched John John's BBQ
+        Sauce, which gained local popularity in Prineville and is now sold in
+        several stores. With aspirations for nationwide distribution, Charles
+        continues to impact his community, having received a Jefferson Award for
+        outstanding public service to youth in Portland.
       </AboutText>
     </StyledAbout>
   );
--- src/components/HighSchool.jsx
+++ src/components/HighSchool.jsx
@@ -26,9 +26,13 @@ const HighSchool = () => {
       <HighSchoolTitle>High School</HighSchoolTitle>
       <HighSchoolBody>
         A graduate of Mountain View High School in Mountain View, California,
-        Charles was a three-year starter in football and baseball. Charles
-        earned All-America honors in football as well as being selected
-        All-League and All-Peninsula in baseball.
+        Charles excelled as a multi-sport athlete, showing remarkable talent in
+        both football and baseball. As a three-year starter in both sports, he
+        earned widespread recognition for his achievements. In football, Charles
+        was a dominant presence on the field, ultimately receiving prestigious
+        All-America honors. His success extended to baseball, where he was a
+        outstanding player, earning All-League and All-Peninsula selections for
+        his exceptional performance.
       </HighSchoolBody>
     </StyledHighSchool>
   );
