4a8d2382f0edc14b485d583617f763b3daac4ec6
Adam Crass <aj.crass@gmail.com>
2025-01-28 08:31:24 -0800

    added padding to airone photos

--- src/components/AirOne.jsx
+++ src/components/AirOne.jsx
@@ -45,6 +45,7 @@ const PictureContainer = styled.div`
 const CroppedImage = styled.img`
   width: 100%;
   height: 150px;
+  padding: 40px;
   object-fit: cover;
   border-radius: 10px;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
